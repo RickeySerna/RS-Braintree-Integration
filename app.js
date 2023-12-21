008f93b2fd6e8772ff9504f90f20c1c78acaf974
Rickey Serna <rickeyserna7@live.com>
2023-12-20 21:57:16 -0600

    Added code to generate 3DS-authenticated Google Pay transactions.

--- app.js
+++ app.js
@@ -62,10 +62,8 @@ app.post('/transaction-with-token', (req, res, next) => {
         verifyCard: true,
         verificationAmount: "1"
       }
-    }/*,
-    deviceData: DeviceDataString*/
-    // Trying to pass device data into the verification as well, but looks like there's an issue with Fraud Protection in sandbox right now.
-    // ah well :D maybe tomorrow.
+    },
+    deviceData: DeviceDataString
   }, (error, result) => {
     if (error) {
       console.error(error);
